a72561c528de402718c64e302ec5800e26cf66d3
SiddhiPranamya <67827905+SiddhiPranamya@users.noreply.github.com>
2020-09-03 20:12:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var value = 0;
+
+function Decrement() {
+  value = value - 1;
+  document.getElementById("p1").innerHTML = value;
+}
+function Increment(){
+  value = value + 1;
+  document.getElementById("p1").innerHTML = value;
+}
